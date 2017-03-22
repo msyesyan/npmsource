1eaf3cf5c90af97c3e9130e011e495d5f55569ba
Qian Chen <channing@Qians-MacBook-Pro.local>
2017-03-22 23:39:12 +0800

    fetch node module by hard code

--- index.js
+++ index.js
@@ -1,2 +1,37 @@
 #!/usr/bin/env node
-console.log('Hello, world!');
+const program = require('commander');
+const shell = require('shelljs');
+const path = require('path');
+const fs = require('fs');
+
+function fetch(module, others=[]) {
+  let modules = [module, ...others].filter(Boolean);
+  if (modules.length === 0) {
+    modules = ['angular'];
+  }
+  fs.existsSync('~/.npmsource', (exists) => {
+    if (!exists) {
+      shell.mkdir('~/.npmsource');
+    }
+  })
+  modules.forEach(module => {
+    shell.exec(`npm view ${module} repository --json`, { async: true, silent: true }, (code, stdout, stderr) => {
+      let repo = stdout && JSON.parse(stdout);
+      if (repo && repo.type === 'git' && repo.url) {
+        shell.exec(`git clone https://${repo.url.split('//')[1]} ~/.npmsource/${module}`, { async: true }, (code, stdout, stderr) => {
+          console.log(stdout);
+        })
+      }
+    })
+  });
+}
+
+program
+  .version('0.0.1')
+
+program
+  .command('fetch [module] [others...]')
+  .description('fetch node module source code')
+  .action(fetch);
+
+program.parse(process.argv);
