dc82bc5d533490e2511843314e6f57b3ad4d285e
msyesyan <msyesyan@gmail.com>
2017-03-23 15:36:44 +0800

    npmsource fetch by package.json

--- index.js
+++ index.js
@@ -7,20 +7,21 @@ const fs = require('fs');
 function fetch(module, others=[]) {
   let modules = [module, ...others].filter(Boolean);
   if (modules.length === 0) {
-    modules = ['angular'];
-  }
-  fs.existsSync('~/.npmsource', (exists) => {
-    if (!exists) {
-      shell.mkdir('~/.npmsource');
+    if (fs.existsSync(path.resolve('./package.json'))) {
+      let packages = require(path.resolve('./package.json'));
+      modules = [...modules, ...Object.keys(packages.dependencies)];
     }
-  })
+  }
+  shell.mkdir('-p', '~/.npmsource');
   modules.forEach(module => {
     shell.exec(`npm view ${module} repository --json`, { async: true, silent: true }, (code, stdout, stderr) => {
       let repo = stdout && JSON.parse(stdout);
       if (repo && repo.type === 'git' && repo.url) {
-        shell.exec(`git clone https://${repo.url.split('//')[1]} ~/.npmsource/${module}`, { async: true }, (code, stdout, stderr) => {
-          console.log(stdout);
-        })
+        shell.exec(
+          `git clone https://${repo.url.split('//')[1]} ~/.npmsource/${module}`,
+          { async: true },
+          (code, stdout, stderr) => { console.log(stdout); }
+        );
       }
     })
   });
@@ -32,6 +33,7 @@ program
 program
   .command('fetch [module] [others...]')
   .description('fetch node module source code')
+  .option('-p, --package [path]', 'file path of package.json', './package.json')
   .action(fetch);
 
 program.parse(process.argv);
