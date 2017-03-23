f0d58bbe8544daafb9c1ef0c7d522b567fdf2a24
msyesyan <msyesyan@gmail.com>
2017-03-23 16:51:36 +0800

    add fetch options

--- index.js
+++ index.js
@@ -4,24 +4,36 @@ const shell = require('shelljs');
 const path = require('path');
 const fs = require('fs');
 
-function fetch(module, others=[]) {
+const homedir = require('os').homedir();
+const npmsourceHome = `${homedir}/.npmsource`;
+
+function fetch(module, others=[], options) {
   let modules = [module, ...others].filter(Boolean);
   if (modules.length === 0) {
-    if (fs.existsSync(path.resolve('./package.json'))) {
+    let packageFile = options.package;
+    if (packageFile && fs.existsSync(path.resolve(packageFile))) {
       let packages = require(path.resolve('./package.json'));
-      modules = [...modules, ...Object.keys(packages.dependencies)];
+      modules = [...modules, ...Object.keys(
+        Object.assign({}, packages.dependencies, (options.all || options.dev) ? packages.devDependencies : {}, (options.all || options.peer) ? packages.peerDependencies : {})
+      )];
     }
   }
-  shell.mkdir('-p', '~/.npmsource');
+  shell.mkdir('-p', npmsourceHome);
   modules.forEach(module => {
     shell.exec(`npm view ${module} repository --json`, { async: true, silent: true }, (code, stdout, stderr) => {
       let repo = stdout && JSON.parse(stdout);
       if (repo && repo.type === 'git' && repo.url) {
-        shell.exec(
-          `git clone https://${repo.url.split('//')[1]} ~/.npmsource/${module}`,
-          { async: true },
-          (code, stdout, stderr) => { console.log(stdout); }
-        );
+        let moduleDir = `${npmsourceHome}/${module}`;
+        if (fs.existsSync(moduleDir)) {
+          shell.cd(moduleDir);
+          shell.exec('git pull');
+        } else {
+          shell.exec(
+            `git clone https://${repo.url.split('//')[1]} ${moduleDir}`,
+            { async: true },
+            (code, stdout, stderr) => { console.log(stdout); }
+          );
+        }
       }
     })
   });
@@ -33,7 +45,10 @@ program
 program
   .command('fetch [module] [others...]')
   .description('fetch node module source code')
-  .option('-p, --package [path]', 'file path of package.json', './package.json')
+  .option('-p, --package [path]', 'file path of package.json')
+  .option('-a, --all', 'fetch all dependencies, work when --package')
+  .option('-d, --dev', 'fetch devDependencies, work when --package')
+  .option('-P, --peer', 'fetch peerDependencies, work when --package')
   .action(fetch);
 
 program.parse(process.argv);
